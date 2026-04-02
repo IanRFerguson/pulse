cb6730b9bc2b0352efd29195d0614d2a00c1458f
Ian <47256454+IanRFerguson@users.noreply.github.com>
2026-04-01 22:13:44 -0400

    clean run

--- src/data-pipeline/load/sources/__init__.py
+++ src/data-pipeline/load/sources/__init__.py
@@ -1,3 +1,4 @@
+from .base import DltSource
 from .dlt_asana import AsanaSource
 from .dlt_freshdesk import FreshdeskSource
 from .dlt_github import GithubSource
--- src/data-pipeline/load/sources/dlt_github.py
+++ src/data-pipeline/load/sources/dlt_github.py
@@ -1,6 +1,25 @@
-from .base import DltSource
+from base import DltSource
+import os
+import requests
+import dlt
 
 #####
+
+GITHUB_TOKEN = os.environ["SOURCES__GITHUB__ACCESS_TOKEN"]
+
+
+@dlt.resource(write_disposition="replace")
+def pull_requests(repo_owner: str, repo_name: str):
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls"
+    headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    yield response.json()
+
+
+@dlt.source
+def github_source():
+    yield pull_requests(repo_owner="move-coop", repo_name="dbt-tmc")
 
 
 class GithubSource(DltSource):
@@ -8,4 +27,23 @@ class GithubSource(DltSource):
     DLT source for GitHub data.
     """
 
-    pass
+    def __init__(self, pipeline_name: str, destination_name: str, dataset_name: str):
+        super().__init__(
+            pipeline_name=pipeline_name,
+            destination_name=destination_name,
+            dataset_name=dataset_name,
+        )
+
+    def load(self, source_data):
+        return super().load(source_data)
+
+
+#####
+
+if __name__ == "__main__":
+    source = GithubSource(
+        pipeline_name="github_pipeline",
+        destination_name="postgres",
+        dataset_name="github_data",
+    )
+    source.load(source_data=github_source())
