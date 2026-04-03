296014671bb1ef74a1b31db444db13236d61444f
Ian <47256454+IanRFerguson@users.noreply.github.com>
2026-04-03 08:12:28 -0400

    github model

--- src/data-pipeline/analytics/models/01_staging/01/stg__01__github.sql
+++ src/data-pipeline/analytics/models/01_staging/01/stg__01__github.sql
@@ -1,23 +1,43 @@
-SELECT
+WITH
+    base AS (
+        SELECT
+            {{
+                dbt_utils.star(
+                    from=ref("stg__00__github_prs")
+                )
+            }}
+        FROM {{ ref("stg__00__github_prs") }}
+    ),
+
+    nested AS (
+        SELECT
+            surrogate_pull_request_id,
+            JSONB_AGG(
+                JSONB_BUILD_OBJECT(
+                    'reviewer_github_username', reviewer_github_username,
+                    'reviewer_github_user_id', reviewer_github_user_id,
+                    'surrogate_pull_request_reviewer_id', surrogate_pull_request_reviewer_id
+                )
+            ) AS reviewers
+        FROM base
+        GROUP BY surrogate_pull_request_id
+    )
+
+SELECT DISTINCT
 
     id,
     github_username,
     github_user_id,
-
     created_at,
     updated_at,
-
     github_repo_name,
     branch_name,
     is_draft,
     github_author_association,
     github_assignee_login,
     github_assignee_id,
-
-    reviewer_github_username,
-    reviewer_github_user_id,
-
-    surrogate_pull_request_id,
-    surrogate_pull_request_reviewer_id
+    reviewers,
+    surrogate_pull_request_id
 
 FROM {{ ref("stg__00__github_prs") }}
+LEFT JOIN nested USING (surrogate_pull_request_id)
